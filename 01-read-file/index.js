990eafc6be381242509de7c4497e80cc1613f404
Anton_Dogadin <dogadin00@gmail.com>
2021-10-31 22:12:07 +0300

    feat: add solution task number 1

---
+++ 01-read-file/index.js
@@ -0,0 +1,10 @@
+const fs = require('fs');
+const path = require('path');
+const stream = require('stream');
+
+let dir = path.join(__dirname, 'text.txt')
+
+let readStream = fs.createReadStream(`${dir}`, 'utf-8')
+
+readStream.on('data', (chunk) => console.log(chunk))
+
