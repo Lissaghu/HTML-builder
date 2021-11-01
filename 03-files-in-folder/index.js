fa512516d7b099ecb353889683d621ea8a341560
Anton_Dogadin <dogadin00@gmail.com>
2021-11-01 19:43:38 +0300

    feat: add solution task number 4

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -4,20 +4,21 @@ const path = require('path');
 let way = path.join(__dirname, 'secret-folder')
 
 fs.readdir(`${way}`, (err, data) => {
+
   data.forEach(item => {
     let way2 = path.join(__dirname, `secret-folder/${item}`)
+
     fs.stat(way2, (err, stats) => {
+
       if (stats.isFile()) {
         console.log(path.basename(item, `${path.extname(item)}`)
           + '  -  '
           + path.extname(item)
           + '  -  '
-          + (stats.size / 1024) + 'kb')
+          + (stats.size / 1024).toFixed(3) + 'kb')
       }
 
     })
-
   })
-
 })
 
--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,50 @@
+const fs = require('fs');
+const path = require('path');
+const fsPromises = require('fs').promises;
 
+let wayOriginalFile = path.join(__dirname, 'files')
+let wayCopyFile = path.join(__dirname, 'files-copy')
+
+function copyDir() {
+
+  fs.mkdir(wayCopyFile, { recursive: true }, (err) => {
+    if (err) {
+      console.log('error in 10 line ' + err)
+    }
+  })
+
+  fs.readdir(wayCopyFile, (err, data) => {
+    if (err) {
+      console.log('error in 16 line ' + err)
+    }
+    data.forEach(item => {
+      fs.unlink(path.join(__dirname, `files-copy/${item}`), (err) => {
+        if (err) throw err
+      })
+    })
+  })
+
+  fs.readdir(wayOriginalFile, (err, data) => {
+
+    if (err) {
+      console.log('error in 27 line ' + err)
+    } else {
+      data.forEach(item => {
+
+        fs.stat(path.join(__dirname, `files/${item}`), (err, stats) => {
+          if (err) {
+            console.log('error in 34 line ' + err)
+          }
+
+          if (stats.isFile()) {
+            fsPromises.copyFile(path.join(__dirname, `files/${item}`), path.join(__dirname, `files-copy/${item}`))
+          }
+        })
+
+      })
+    }
+
+  })
+
+}
+copyDir()
