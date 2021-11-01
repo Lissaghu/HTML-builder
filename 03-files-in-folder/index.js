ef2e64f51789d88ce4bb5fdd9064b8222536239d
Anton_Dogadin <dogadin00@gmail.com>
2021-11-01 14:47:19 +0300

    feat: add solution task number 3

---
+++ 03-files-in-folder/index.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const path = require('path');
+
+let way = path.join(__dirname, 'secret-folder')
+
+fs.readdir(`${way}`, (err, data) => {
+  data.forEach(item => {
+    let way2 = path.join(__dirname, `secret-folder/${item}`)
+    fs.stat(way2, (err, stats) => {
+      if (stats.isFile()) {
+        console.log(path.basename(item, `${path.extname(item)}`)
+          + '  -  '
+          + path.extname(item)
+          + '  -  '
+          + (stats.size / 1024) + 'kb')
+      }
+
+    })
+
+  })
+
+})
+
